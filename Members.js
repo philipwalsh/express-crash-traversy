dea1dd4ef0005820110d88cfdf82d21bcccf06d1
Philip Walsh <philipwalsh.ds@gmail.com>
2020-12-23 09:07:58 -0500

    add logger middleware

---
+++ Members.js
@@ -0,0 +1,17 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Mike Williams',
+        status: 'deadbeat'
+    },    {
+        id: 3,
+        name: 'Lucy Lawless',
+        status: 'active'
+    }
+]
+module.exports = members;
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require('path');
-
+const members=require('./Members')
+const moment = require('moment')
 
 // function keyword instead of arrow function
 // app.get('/', function(req, res){
@@ -14,6 +15,22 @@ const path = require('path');
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
+
+
+// create a middleware function
+logger = (req, res, next) =>{
+    console.log(`${req.protocol}:${req.get('host')}${req.originalUrl}: ${moment().format()}`);
+    next();
+}
+
+app.use(logger);
+
+app.get('/api/members', (req,res) =>{
+    res.json(members);
+});
+//or could have written it this way, no curly vbraces needed
+app.get('/api/members2', (req,res) => res.json(members));
+
 
 
 //set a static folder
